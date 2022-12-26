93ab56e232c5ca25396d2565d0fd6734ec618d0a
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2022-12-26 22:49:44 +0300

    app: main: Clean up main module implementation

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::fmt::Debug;
+
+use log::error;
+use teloxide::prelude::*;
+
+use bot::setup_bot;
+use db::{db_task, DbCommand};
+
+mod bot;
+mod db;
+
+#[derive(Debug)]
+pub(self) struct ApplicationCommand<T> {
+    db_cmd: DbCommand,
+    tx_channel: tokio::sync::oneshot::Sender<T>,
+}
+
+impl<T> ApplicationCommand<T> {
+    fn new(db_cmd: DbCommand, tx_channel: tokio::sync::oneshot::Sender<T>) -> Self {
+        ApplicationCommand { db_cmd, tx_channel }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let tgbot = Bot::from_env();
+    let (tx, rx) = tokio::sync::mpsc::channel::<ApplicationCommand<String>>(16);
+
+    if let Err(e) = tokio::try_join!(setup_bot(tgbot, tx), db_task(rx)) {
+        error!("{}", e);
+    }
+}
