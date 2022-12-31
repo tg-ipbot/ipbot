26d489a9913a068cbf01b8a06843b4b2793b84cc
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2022-12-31 23:23:38 +0300

    app: rest: Clean up warnings

--- src/rest.rs
+++ src/rest.rs
@@ -2,9 +2,12 @@ use std::convert::Infallible;
 use std::net::IpAddr;
 use std::str::FromStr;
 
+use log::{debug, warn};
 use warp::http::StatusCode;
 use warp::hyper::body::Bytes;
 use warp::{Filter, Rejection};
+
+use crate::db::{AppIpReport, DbCommand};
 
 use super::ApplicationCommand;
 
@@ -18,30 +21,52 @@ pub(super) async fn run_server(
     Ok(())
 }
 
-async fn request_handler(id: String, addr: IpAddr) -> Result<impl warp::Reply, Infallible> {
-    if let IpAddr::V6(_) = addr {
+async fn request_handler(
+    id: String,
+    addr: IpAddr,
+    cmd_tx: tokio::sync::mpsc::Sender<ApplicationCommand<String>>,
+) -> Result<impl warp::Reply, Infallible> {
+    if addr.is_ipv6() {
         return Ok(StatusCode::NOT_ACCEPTABLE);
     }
 
-    println!("ID: {id}: {addr}");
-    Ok(StatusCode::OK)
-}
+    let (tx, rx) = tokio::sync::oneshot::channel::<String>();
+    debug!("ID: {id}: {addr}");
 
-fn app_request_handler(id: u32) -> String {
-    format!("app:{}", id)
+    match AppIpReport::from_str(id.as_str(), addr) {
+        Ok(report) => {
+            match cmd_tx
+                .send(ApplicationCommand::new(DbCommand::IpReport(report), tx))
+                .await
+            {
+                Ok(_) => {
+                    let _ = rx.await;
+                }
+                Err(e) => {
+                    warn!("{e}");
+                    return Ok(StatusCode::INTERNAL_SERVER_ERROR);
+                }
+            }
+
+            Ok(StatusCode::OK)
+        }
+        Err(e) => {
+            warn!("Report create error: {e}");
+            Ok(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 fn init_rest(
-    _tx: tokio::sync::mpsc::Sender<ApplicationCommand<String>>,
+    tx: tokio::sync::mpsc::Sender<ApplicationCommand<String>>,
 ) -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone + Send + Sync {
-    let app_path = warp::path("app")
+    warp::path("app")
         .and(warp::post())
-        .and(warp::path::param().map(app_request_handler))
+        .and(warp::header::header::<String>("Credential"))
         .and(warp::body::bytes().and_then(|data: Bytes| async move {
             IpAddr::from_str(String::from_utf8_lossy(data.as_ref()).as_ref())
                 .map_err(|_e| warp::reject::not_found())
         }))
-        .and_then(request_handler);
-
-    app_path
+        .and(warp::any().map(move || tx.clone()))
+        .and_then(request_handler)
 }
